98844cba2b4ec50903b0fb5ab0e6fbefc4ae417a
Miguel Galindo <miguelgalindo@miguels-mbp.myfiosgateway.com>
2023-07-11 20:56:48 -0400

    Adding code explanation

--- index.php
+++ index.php
@@ -51,3 +51,13 @@ echo "Circle Area: " . $circle->getArea() . PHP_EOL;
 
 $rectangle = new Rectangle("Blue", 4, 6);
 echo "Rectangle Area: " . $rectangle->getArea() . PHP_EOL;
+
+?>
+
+In this example, we have an abstract class Shape that defines a common behavior getArea() for different shapes. The Shape class has an abstract method getArea() which must be implemented by its derived classes.
+
+The Circle class and Rectangle class both extend the Shape class and provide their own implementations for the getArea() method. They also have their own properties (radius and width, height) and constructors.
+
+In the main part of the code, we create instances of Circle and Rectangle, passing the required parameters to their constructors. We then call the getArea() method on each object to calculate and display the respective areas of the shapes.
+
+By using abstract classes and methods, we can define a common interface (getArea()) that must be implemented by any class that wants to be considered a shape. This promotes code reusability and ensures that each shape has its own implementation for calculating its area.
